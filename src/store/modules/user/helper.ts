4ec7a83b044a54e2d5ce51363cbaa961db762446
cxs <chengxs1994@hotmail.com>
2023-04-05 22:48:24 +0800

    fix: xs

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://avatars.githubusercontent.com/u/33446363?s=40&v=4',
+      name: 'XS',
+      description: 'Star on <a href="https://github.com/cxsgit/chatgpt-web-xs" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
